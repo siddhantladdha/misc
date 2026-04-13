d357700cf4dd8aaf1204a0c205bb5fc8971268e4
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-04-12 20:45:53 -0400

    create python scripts

---
+++ .github/workflows/scripts/hello.py
@@ -0,0 +1,10 @@
+# /// script
+# dependencies = [
+#   "plumbum",
+# ]
+# requires-python = ">=3.14"
+# ///
+
+from plumbum.cmd import echo
+echo("Hello world from plumbum echo")
+print("Hello world from python print")
