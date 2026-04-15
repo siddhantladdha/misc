672343c16b3b9626516e2c1fa79e2675516bb58e
Siddhant Laddha <25705822+siddhantladdha@users.noreply.github.com>
2026-04-15 10:04:41 -0400

    init

---
+++ shit-container/almalinux_8p10/python/appimage_build.py
@@ -0,0 +1,31 @@
+# /// script
+# dependencies = [
+#   "plumbum",
+#   "rich",
+# ]
+# requires-python = ">=3.14"
+# ///
+
+from plumbum.cmd import chmod
+from plumbum import local
+from plumbum.path.utilities import copy, move
+from rich import print
+
+local.cwd.chdir("python_build")
+
+print('Extracting appimages.')
+
+appy = ["python", "appimagetool"]
+for app in appy:
+    app_cmd = local.get("./" + app)
+    app_cmd("--appimage-extract")
+    move("squashfs-root", app + ".AppDir")
+
+copy("./requirements.txt", "python.AppDir")
+local.cwd.chdir("python.AppDir")
+print('Starting pip install from requirements.txt')
+#./usr/bin/pip install -r ./requirements.txt
+#./usr/bin/pip freeze --requirement requirements.txt > frozen_requirements.txt
+#set -x ARCH x86_64
+#cd ..
+#./appimagetool.AppDir/AppRun ./python.AppDir python
